faeb627368b710bd5cbeed1cb9bb917e11dd7f48
sanxiyn <sanxiyn@f224b4e6-8335-0410-baf7-2d8196006633>
2007-10-30 06:58:10 +0000

    Test for Date.parse
    git-svn-id: http://rubydotnetcompiler.googlecode.com/svn/trunk@153 f224b4e6-8335-0410-baf7-2d8196006633

--- tests/test.rb
+++ tests/test.rb
@@ -20,10 +20,14 @@ test_ok(CSV.parse_line(line).join(',') == line)
 require 'rational'
 test_ok(1.quo(2) * 2 == 1)
 
-require 'uri/generic'
+require 'uri'
 uri = 'http://example.com/'
 test_ok(URI.parse(uri).to_s == uri)
 
 require 'cgi'
 string = 'Hello, world!'
 test_ok(CGI.unescape(CGI.escape(string)) == string)
+
+require 'date'
+string = '1985-12-23'
+test_ok(Date.parse(string).to_s == string)
